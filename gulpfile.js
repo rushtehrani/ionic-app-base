918b750f3e772d6d6e7a014ac5a56d4dfde821b6
rushtehrani <r@inven.io>
2014-09-14 19:29:41 -0700

    more app base refactors.

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,10 @@ var gulp = require('gulp'),
     less = require('gulp-less'),
     minifyCss = require('gulp-minify-css'),
     sh = require('shelljs'),
-    wiredep = require('wiredep').stream;
+    wiredep = require('wiredep').stream,
+    source = require('vinyl-source-stream'),
+    browserify = require('browserify');
+
 
 var paths = {
   js: ['./www/**/*.js'],
@@ -15,25 +18,32 @@ var paths = {
 gulp.task('less', function() {
   gulp.src(paths.css)
     .pipe(less())
-    .pipe(concat('bundle.css'))
+    .pipe(concat('./bundle.css'))
     .pipe(gulp.dest('./www/'));
 });
 
 gulp.task('watch', function() {
   gulp.watch(paths.css, ['less']);
+  gulp.watch(paths.js, ['browserify']);
 });
 
 gulp.task('bower', function() {
   bower.commands.install()
     .on('end', function() {
-      gulp.src('www/index.html')
-        .pipe(wiredep())
-        .pipe(gulp.dest('www'));
+      gulp.src('./www/index.html')
+        .pipe(wiredep({ devDependencies: true }))
+        .pipe(gulp.dest('./www/'));
     })
     .on('log', function(data) {
       gutil.log('bower', gutil.colors.cyan(data.id), data.message);
     });
 });
+
+gulp.task('browserify', function() {
+  browserify('./www/app.js').bundle()
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest('./www/'))
+})
 
 gulp.task('git-check', function(done) {
   if (!sh.which('git')) {
